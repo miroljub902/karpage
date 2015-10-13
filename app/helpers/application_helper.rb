220efed085549ae89ae6881a62892c8ddb8cf790
Ivan Vega <ivanvega@gmail.com>
2015-10-12 22:00:32 -0500

    Footer.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def copyright_notice
+    years = (2015..Date.today.year).to_a.map(&:to_s)
+    years = [ years.first, years.last ].uniq.join('-')
+    "@ #{years} Nix. All Rights Reserved."
+  end
 end
