0f8df6987a1886fb9cfbc619af3e526b2a7bbd30
Ivan Vega <ivanvega@gmail.com>
2015-10-12 22:40:21 -0500

    Fix cells escaping HTML on production.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,15 @@ module ApplicationHelper
     years = [ years.first, years.last ].uniq.join('-')
     "@ #{years} Nix. All Rights Reserved."
   end
+
+  # There's a bug that in production causes the cell's contents
+  # to return as escaped HTML, so we have to explicitly call ".()"
+  # Also allows block render-yield as a convenience
+  def cell(name, model = nil, options = {})
+    options = options.dup
+    options.merge!(content: -> {
+      capture { yield }
+    }) if block_given?
+    controller.cell(name, model, options).()
+  end
 end
