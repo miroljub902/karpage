f91a63292a0be87d114be24326459f8e159706c6
Ivan Vega <ivanvega@gmail.com>
2015-10-12 21:43:15 -0500

    Car list on home page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,14 @@
 class HomeController < ApplicationController
   def index
+    owners = [%w(John Michael Steven Angelina), %w(Doe Leery Tyler Solano)]
+    models = [ 'Mazda CX-7', 'Chevrolet Corvette', 'Bugatti Veyron', 'Ferrari 458', 'Porsche Carrera', 'Lamborghini Aventador' ]
+    locations = [ 'Chicago, IL', 'Miami, FL', 'Manhattan, NY', 'Dallas, TX', 'California, LA' ]
+    @cars = 5.times.map do
+      OpenStruct.new(
+        owner: "#{owners.first.sample} #{owners.last.sample}",
+        name: "#{rand(15) + 2000} #{models.sample}",
+        location: locations.sample
+      )
+    end
   end
 end
