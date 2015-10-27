c8c71dbaeb6ab9ee5592222a6c59b268561c5a5f
Ivan Vega <ivanvega@gmail.com>
2015-10-27 15:38:54 -0600

    Don't count own user's visits.

--- app/controllers/profile_cars_controller.rb
+++ app/controllers/profile_cars_controller.rb
@@ -2,6 +2,6 @@ class ProfileCarsController < ApplicationController
   def show
     user = User.find_by(login: params[:profile_id])
     @car = user.cars.friendly.find(params[:car_id])
-    @car.increment! :hits
+    @car.increment! :hits unless @car.user_id == current_user.try(:id)
   end
 end
