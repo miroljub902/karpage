d6677bd8379f679e8607d75c8f76a5392da915a2
Ivan Vega <ivanvega@gmail.com>
2015-10-28 16:35:50 -0600

    Fix comment policy on guest users.

--- app/policies/comment_policy.rb
+++ app/policies/comment_policy.rb
@@ -1,6 +1,6 @@
 class CommentPolicy < ApplicationPolicy
   def destroy?
-    record.user_id == user.id || record.commentable.user_id == user.id
+    user && (record.user_id == user.id || record.commentable.user_id == user.id)
   end
 
   class Scope < Scope
