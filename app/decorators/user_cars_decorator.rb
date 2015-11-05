9abe04e9f342c9dd7f2c649bccf1a484afc5a949
Ivan Vega <ivanvega@gmail.com>
2015-11-05 00:04:52 -0600

    Bugfix.

--- app/decorators/user_cars_decorator.rb
+++ app/decorators/user_cars_decorator.rb
@@ -2,8 +2,9 @@ class UserCarsDecorator
   class << self
     def cars(user)
       cars = user.cars
+      first_car = cars.detect(&:first?)
       OpenStruct.new(
-        first:      UserCarDecorator.new(cars.detect(&:first?)),
+        first:      first_car ? UserCarDecorator.new(first_car) : nil,
         current:    cars.find_all(&:current?).map { |car| UserCarDecorator.new(car) },
         past:       cars.find_all(&:past?).map { |car| UserCarDecorator.new(car) }
       )
