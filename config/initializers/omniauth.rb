924b4e4a0b0a394a3e03a82846a54129721b873e
Ivan Vega <ivanvega@gmail.com>
2015-12-02 15:32:46 -0600

    Log Facebook App Id.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+  Rails.logger.info "Facebook App Id: #{ENV['FACEBOOK_APP_ID']}"
   provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'], image_size: 'large'
 end
