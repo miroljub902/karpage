5a6b0324200e8994399d7537f048b7f36e29dd69
Ivan Vega <ivanvega@gmail.com>
2015-12-02 15:37:51 -0600

    Expire assets.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  Rails.logger.info "Facebook App Id: #{ENV['FACEBOOK_APP_ID']}"
   provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'], image_size: 'large'
 end
