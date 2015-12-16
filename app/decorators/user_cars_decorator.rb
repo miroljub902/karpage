e53f92b4db18e19f5cce7ee56beeb15def53605d
Ivan Vega <ivanvega@gmail.com>
2015-12-16 10:25:41 -0600

    Sort current cars by date.

--- app/decorators/user_cars_decorator.rb
+++ app/decorators/user_cars_decorator.rb
@@ -5,7 +5,7 @@ class UserCarsDecorator
       first_car = cars.detect(&:first?)
       OpenStruct.new(
         first:   first_car ? UserCarDecorator.new(first_car) : nil,
-        current: cars.find_all(&:current?).map { |car| UserCarDecorator.new(car) },
+        current: cars.find_all(&:current?).sort_by { |car| car.created_at }.reverse.map { |car| UserCarDecorator.new(car) },
         past:    cars.find_all(&:past?).sort_by { |car| car.created_at }.reverse.map { |car| UserCarDecorator.new(car) }
       )
     end
