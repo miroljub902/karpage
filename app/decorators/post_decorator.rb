1b776ebdeb02738c58c29d4ac20a55f75068aeb4
Ivan Vega <ivanvega@gmail.com>
2016-01-27 22:18:09 -0600

    Remove astray comment

--- app/decorators/post_decorator.rb
+++ app/decorators/post_decorator.rb
@@ -5,7 +5,6 @@ class PostDecorator < Draper::Decorator
   def truncated_body(length: 400)
     more = h.link_to('Read more', h.post_path(user, self))
     if post.body.size > length
-      # simple_format auto_link(truncate(post.body, length: length), html: { target: '_blank' })
       html_body h.truncate(post.body, length: length) + " #{more}".html_safe
     else
       html_body
