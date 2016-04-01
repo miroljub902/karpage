13422c13cbaf25a0c06a9b7842b892ee48824b7c
Ivan Vega <ivanvega@gmail.com>
2016-04-01 16:53:21 -0600

    Add get app button to navigation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
-    if (cookies[:splash] && !browser.device.mobile?) || params[:h] == '1'
+    force_splash = params[:m] == "1"
+    if !force_splash && ((cookies[:splash] && !browser.device.mobile?) || params[:h] == '1')
       @cars = Car.featured.limit(6).presence || Car.popular.limit(6)
       @users = (User.featured.limit(4).presence || User.by_cars_owned.limit(4)).decorate
     else
