393c3c544d630e36dc2531d8a660a315a1d92d18
Ivan Vega <ivanvega@gmail.com>
2016-05-07 20:22:09 -0500

    Small fix

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,6 +1,6 @@
 class FollowsController < ApplicationController
   def index
-    @user = User.find_by(login: params[:profile_id]).decorate
+    @user = User.find_by!(login: params[:profile_id]).decorate
     @users = if params[:followers]
       reset_new_stuff @user.follows_by, owner: @user
       @user.followers.order('follows.created_at DESC')
