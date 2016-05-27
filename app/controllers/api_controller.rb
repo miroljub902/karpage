042d7eea8d1d3ffb408469e151ece4d8cb191f42
Ivan Vega <ivanvega@gmail.com>
2016-05-27 14:45:14 -0500

    Allow extra CORS headers

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -37,7 +37,7 @@ class ApiController < ActionController::Base
   def cors_set_headers
     headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
-    headers['Access-Control-Allow-Headers'] = 'Authorization, Content-Type, X-Requested-With, X-Prototype-Version, Token, Origin, Accept'
+    headers['Access-Control-Allow-Headers'] = 'Authorization, Content-Type, X-Requested-With, X-Prototype-Version, User-Agent, X-Resolution, Token, Origin, Accept'
     headers['Access-Control-Max-Age'] = '1728000'
   end
 end
