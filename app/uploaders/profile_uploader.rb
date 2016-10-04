790d1a09a04a7fa0813c814184ca9a202c6aa8de
psalmuelaguilar <psalmuelaguilar@gmail.com>
2016-10-05 01:54:57 +0800

    Modification on profile thumbnail

--- app/uploaders/profile_uploader.rb
+++ app/uploaders/profile_uploader.rb
@@ -19,9 +19,6 @@ class ProfileUploader
         MiniMagick::Image.new('app/assets/images/profile/header-bg.jpg')
       end
     header_image.resize "1000x420^"
-    header_image = header_image.composite(transparent_cover) do |c|
-      c.compose "DstOver"
-    end
   end
 
   def image_profile_picture
@@ -41,10 +38,11 @@ class ProfileUploader
       if cars = @user.cars
         car = cars.where(sorting: 0).first
         car_image = MiniMagick::Image.open('https://'+ ENV.fetch('IMGIX_SOURCE') + "/store/#{car.photos.sorted.first.image_id}")
+        car_image.resize "1000x420^" # this is just to make sure this image is converted before writing the text
         car_image.combine_options do |i|
           i.font "app/assets/fonts/helvetica.ttf"
           i.gravity "Center"
-          i.pointsize 50
+          i.pointsize 80
           i.fill 'White'
           i.draw "text 0,0 '#{car.year} #{car.make.name} #{car.model.name}'"
         end
@@ -54,10 +52,8 @@ class ProfileUploader
 
     car_image.combine_options do |i|
       i.colorspace 'Gray'
-      i.gamma '0.8'
+      i.gamma '0.5'
     end
-
-    car_image.resize "1000x420^"
   end
 
   def name_container
@@ -74,9 +70,12 @@ class ProfileUploader
     end
   end
 
-  def transparent_cover
-    cover = MiniMagick::Image.new('app/assets/images/profile/transparent_cover.png')
-    cover.resize "1000x420^"
+  def counter_container
+    'app/assets/images/profile/counter_container.png'
+  end
+
+  def counter_template
+    'app/assets/images/profile/counter_template.png'
   end
 
   def profile_image_generator
@@ -86,68 +85,104 @@ class ProfileUploader
     blank_base = MiniMagick::Image.new(blank_base_image)
     template = MiniMagick::Image.new(image_template)
     profile_template_image = MiniMagick::Image.new(profile_template)
+    follower_counter = MiniMagick::Image.new(counter_container)
+    car_counter = MiniMagick::Image.new(counter_container)
+    followee_counter = MiniMagick::Image.new(counter_container)
+    template_counter = MiniMagick::Image.new(counter_template)
+
     name_template = name_container
 
-    profile_image.resize "340x270^"
+    profile_image.resize "335^x265"
 
     result = blank_base.composite(header_image) do |c|
       c.compose "Over"
-      c.geometry "+745+165"
+      c.geometry "+713+165"
     end
 
     result = result.composite(car_image) do |c|
       c.compose "Over"
-      c.geometry "+745+1200"
+      c.geometry "+713+1200"
     end
 
     created_profile_image = profile_template_image.composite(profile_image) do |c|
-      c.compose "In"
+      c.compose "atop"
+      c.gravity "center"
     end
 
     result = result.composite(created_profile_image) do |c|
       c.compose "Over"
-      c.geometry "+1075+455"
+      c.geometry "+1046+455"
     end
 
     result = result.composite(template) do |c|
       c.compose "Over"
     end
+    #### Counter Templating ####
 
-    result.combine_options do |i|
-      i.font "app/assets/fonts/helvetica.ttf"
-      i.gravity "Center"
-      i.pointsize 50
-      i.draw "text -117,-12 ' #{@user.followers.count} '"
+    ### follower Counter
+    follower_counter = template_counter.composite(follower_counter) do |c|
+      c.compose "Over"
     end
 
-    result.combine_options do |i|
+    follower_counter.combine_options do |i|
       i.font "app/assets/fonts/helvetica.ttf"
       i.gravity "Center"
       i.pointsize 50
-      i.draw "text 227,-12 '2'"
+      i.draw "text 0,0 ' #{@user.followers.count} '"
     end
 
-    result.combine_options do |i|
+    result = result.composite(follower_counter) do |c|
+      c.compose "Over"
+      c.geometry "+718+985"
+    end
+
+    ### Car Counter
+    car_counter = template_counter.composite(car_counter) do |c|
+      c.compose "Over"
+    end
+
+    car_counter.combine_options do |i|
       i.font "app/assets/fonts/helvetica.ttf"
       i.gravity "Center"
       i.pointsize 50
-      i.draw "text 557,-12 '#{@user.followees.count}'"
+      i.draw "text 0,0 '#{@user.cars.count}'"
+    end
+
+    result = result.composite(car_counter) do |c|
+      c.compose "Over"
+      c.geometry "+1055+985"
+    end
+
+    ### Followee counter
+    followee_counter = template_counter.composite(followee_counter) do |c|
+      c.compose "Over"
+    end
+
+    followee_counter.combine_options do |i|
+      i.font "app/assets/fonts/helvetica.ttf"
+      i.gravity "Center"
+      i.pointsize 50
+      i.draw "text 0,0 '#{@user.followees.count}'"
+    end
+
+    result = result.composite(followee_counter) do |c|
+      c.compose "Over"
+      c.geometry "+1390+985"
     end
 
     result = result.composite(name_template) do |c|
       c.compose "Over"
-      c.geometry "+745+725"
+      c.geometry "+713+725"
     end
 
-    result.path
+    #### Counter Templating ####
+
     result.write 'public/images/temp_thumbnail_image.jpg'
-    # with a regular File object
+
     File.open(result.path, "rb") do |file|
       @user.profile_thumbnail = file
     end
 
     @user.save
   end
-  # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
 end
