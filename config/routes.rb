fc9b89b28c199a5847ae907f5185d8a9f4aca86a
Ivan Vega <ivanvega@gmail.com>
2016-11-09 00:30:01 -0600

    Sidekiq web

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    username == ENV['SIDEKIQ_USERNAME'] && password == ENV['SIDEKIQ_PASSWORD']
+  end if Rails.env.production?
+
+  mount Sidekiq::Web => '/sidekiq'
+
   ActiveAdmin.routes(self)
 
   namespace :api, constraints: { subdomain: ENV.fetch('API_SUBDOMAIN') } do
