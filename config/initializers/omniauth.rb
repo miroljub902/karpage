da4bc6554f6b1e4f9715fa34462c6c95b58d2e57
Ivan Vega <ivanvega@gmail.com>
2017-02-07 12:41:58 -0600

    Upgrade Facebook login API version

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'], image_size: 'large'
+  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'], image_size: 'large',
+           client_options: {
+             site:          'https://graph.facebook.com/v2.8',
+             authorize_url: 'https://www.facebook.com/v2.8/dialog/oauth'
+           }
 end
