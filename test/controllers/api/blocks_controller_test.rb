fb79a1fbda7cc99d4836c3fdc122bf5c11e9dcd2
Ivan Vega <ivanvega@gmail.com>
2017-03-21 22:02:04 -0600

    Small style fix

--- test/controllers/api/blocks_controller_test.rb
+++ test/controllers/api/blocks_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 require_relative '../api_controller_test'
 
 class Api::BlocksControllerTest < ApiControllerTest
-test 'can block user' do
+  test 'can block user' do
     user = users(:john_doe)
     fiend = users(:friend)
     assert_difference 'user.blocks.count' do
