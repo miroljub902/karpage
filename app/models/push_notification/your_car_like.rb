090ee487560857c26eb8197e3b446d02f6df7657
Ivan Vega <ivanvega@gmail.com>
2017-05-17 13:32:32 -0500

    Fix notification

--- app/models/push_notification/your_car_like.rb
+++ app/models/push_notification/your_car_like.rb
@@ -7,12 +7,4 @@ class PushNotification::YourCarLike < PushNotification
     photo = notifiable.photos.sorted.first
     photo ? ix_refile_image_url(photo, :image) : nil
   end
-
-  def related_id
-    notifiable.commentable_id
-  end
-
-  def related_type
-    'Car'
-  end
 end
