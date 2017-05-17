de279f6fa88808f4ccb9fca626143ec575e4c9c9
Ivan Vega <ivanvega@gmail.com>
2017-05-16 20:33:06 -0500

    Fix notifications API pagination

--- app/controllers/api/notifications_controller.rb
+++ app/controllers/api/notifications_controller.rb
@@ -2,7 +2,7 @@ class Api::NotificationsController < ApiController
   before_action :require_user
 
   def index
-    @notifications = current_user.notifications.page(params[:page]).per(10)
+    @notifications = current_user.notifications.page(params[:page]).per(params[:per] || 10)
     respond_with :api, @notifications
   end
 end
