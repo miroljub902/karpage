aeda2594e274bdaa9cfe2199553bf06639ca27f6
Ivan Vega <ivanvega@gmail.com>
2017-05-24 11:39:34 -0500

    More notification logging

--- app/jobs/push_notification_job.rb
+++ app/jobs/push_notification_job.rb
@@ -3,6 +3,9 @@ class PushNotificationJob < ActiveJob::Base
 
   def perform(notification_id)
     notification = Notification.includes(:user, :notifiable, :source).find_by(id: notification_id)
+    # TODO: Remove logging
+    Appsignal.send_error(StandardError.new("Notification not found: #{notification_id}")) unless notification
+    notification.update_attribute :status_message, "Pushing... #{notification.unsent?}" if notification
     notification.push! if notification&.unsent?
   end
 end
