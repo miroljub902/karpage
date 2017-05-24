a4509e226c46f28fb507e3cd2a6ec4524987f2c6
Ivan Vega <ivanvega@gmail.com>
2017-05-24 11:31:17 -0500

    (API) Return notifications by most-recent

--- app/controllers/api/notifications_controller.rb
+++ app/controllers/api/notifications_controller.rb
@@ -2,7 +2,7 @@ class Api::NotificationsController < ApiController
   before_action :require_user
 
   def index
-    @notifications = current_user.notifications.page(params[:page]).per(params[:per] || 10)
+    @notifications = current_user.notifications.recent.page(params[:page]).per(params[:per] || 10)
     respond_with :api, @notifications
     reset_new_stuff @notifications, owner: current_user
   end
