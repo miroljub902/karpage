5dd6e0f1c284834206c77c685d4d791f0d5f72ba
Ivan Vega <ivanvega@gmail.com>
2017-06-23 15:39:37 -0500

    Fix filters

--- app/models/filter.rb
+++ app/models/filter.rb
@@ -1,7 +1,7 @@
 class Filter < ActiveRecord::Base
   def search
     words = self.words.presence || name
-    sql = words.split(",").map do |word|
+    sql = words.split(',').map(&:strip).map do |word|
       "cars.slug ILIKE '%#{word}%' OR makes.name ILIKE '%#{word}%' OR models.name ILIKE '%#{word}%'"
     end
     Car.has_photos.owner_has_login.joins(:make, :model).where(sql.join(" OR "))
