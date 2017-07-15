a8fe4301cea43f4f4a7d0114dd28db44d83783dd
Ivan Vega <ivanvega@gmail.com>
2017-07-14 22:11:53 -0500

    Configurable Notification.belay_create throttle

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -28,7 +28,7 @@ class Notification < ActiveRecord::Base
   def self.belay_create(user:, source:, type:, notifiable:)
     return if source == user # Skip notifications to "myself"
     last_created_at = user.notifications.recent.where(type: type).first&.created_at
-    return if last_created_at && last_created_at > 1.minute.ago
+    return if last_created_at && last_created_at > (ENV['NOTIFICATION_THROTTLE'].presence || 1).to_i.minutes.ago
     Notification.create! user: user, source: source, type: type, notifiable: notifiable
   end
 
