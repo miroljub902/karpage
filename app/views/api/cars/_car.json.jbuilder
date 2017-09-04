1bc64ae1bcd3ea08255ef3fc02a597633ebfbec9
Ivan Vega <ivanvega@gmail.com>
2017-09-04 17:17:35 -0500

    (API) Return HTML formatted car description

--- app/views/api/cars/_car.json.jbuilder
+++ app/views/api/cars/_car.json.jbuilder
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 UserCarDecorator.new(car).tap do |car|
-  json.call(car,
-            :id, :year, :make_id, :model_id, :trim_id, :description, :created_at, :updated_at, :likes_count, :sorting)
+  json.call(car, :id, :year, :make_id, :model_id, :trim_id, :created_at, :updated_at, :likes_count, :sorting)
+
+  json.description simple_format(car.description, {}, wrapper_tag: :div)
 
   json.make car.make&.name
   json.model car.object.model.name # car.model gets a Car since it's a decorator
--- test/controllers/api/cars_controller_test.rb
+++ test/controllers/api/cars_controller_test.rb
@@ -25,7 +25,7 @@ class Api::CarsControllerTest < ApiControllerTest
     car = user.cars.first
     car.photos.create! image_id: 'dummy'
     car.comments.create! user: users(:friend), body: 'Nice car'
-    get :index, params: { search: 'audi' }
+    get :index, search: 'audi'
     assert_response :ok
   end
 
@@ -39,7 +39,7 @@ class Api::CarsControllerTest < ApiControllerTest
     car2.photos.create! image_id: 'dummy2'
     user.cars << car1
     user.cars << car2
-    get :index, params: { filter_id: filter.id }
+    get :index, filter_id: filter.id
     assert_response :ok
     assert_equal 1, json_response['cars'].size
     assert_equal car1.id, json_response['cars'].first['id']
@@ -49,7 +49,7 @@ class Api::CarsControllerTest < ApiControllerTest
     user = users(:john_doe)
     authorize_user user
     assert_difference 'user.cars.count' do
-      post :create, params: { car: { year: '2015', make_name: 'Audi', car_model_name: 'R8' } }
+      post :create, car: { year: '2015', make_name: 'Audi', car_model_name: 'R8' }
       assert_response :created
     end
   end
@@ -59,7 +59,7 @@ class Api::CarsControllerTest < ApiControllerTest
     user.cars << cars(:current)
     car = user.cars.first
     authorize_user user
-    patch :update, params: { id: car.id, car: { description: 'Updated' } }
+    patch :update, id: car.id, car: { description: 'Updated' }
     assert_response :no_content
     assert_equal 'Updated', car.reload.description
   end
@@ -69,8 +69,19 @@ class Api::CarsControllerTest < ApiControllerTest
     user.cars << cars(:current)
     car = user.cars.first
     authorize_user user
-    get :show, params: { id: car.id }
+    get :show, id: car.id
     assert_response :ok
+  end
+
+  test 'returns formatted description' do
+    user = users(:john_doe)
+    user.cars << cars(:current)
+    car = user.cars.first
+    car.update_attribute :description, "Description with\nline break"
+    authorize_user user
+    get :show, id: car.id
+    assert_response :ok
+    assert_match(/<br/, json_response['car']['description'])
   end
 
   test 'returns whats new' do
@@ -83,7 +94,7 @@ class Api::CarsControllerTest < ApiControllerTest
     Like.like! car, friend
     car.comments.create! user: friend, body: 'Howdy'
     authorize_user user
-    get :show, params: { id: car.id }
+    get :show, id: car.id
     assert_equal 1, json_response['car']['new_likes']
     assert_equal 1, json_response['car']['new_comments']
   end
@@ -93,7 +104,7 @@ class Api::CarsControllerTest < ApiControllerTest
     user.cars << cars(:current)
     car = user.cars.first
     authorize_user user
-    delete :destroy, params: { id: car.id }
+    delete :destroy, id: car.id
     assert_response :no_content
     assert_raise { car.reload }
   end
@@ -107,7 +118,7 @@ class Api::CarsControllerTest < ApiControllerTest
     photo = car.photos.first
     authorize_user user
     stub_request :delete, /s3/
-    patch :update, params: { id: car.id, car: { photos_attributes: { id: photo.id, _destroy: true } } }
+    patch :update, id: car.id, car: { photos_attributes: { id: photo.id, _destroy: true } }
     assert_response :no_content
     assert_equal 1, car.reload.photos.count
     assert_not_equal photo.id, car.photos.first.id
@@ -116,7 +127,7 @@ class Api::CarsControllerTest < ApiControllerTest
   test 'can add with parts list' do
     user = users(:john_doe)
     authorize_user user
-    post :create, params: { car: {
+    post :create, car: {
       year: '2015', make_name: 'Audi', car_model_name: 'R8',
       parts_attributes: [
         { type: 'Wheel', manufacturer: 'Michelin', model: 'X100', price: 500,
@@ -128,7 +139,7 @@ class Api::CarsControllerTest < ApiControllerTest
           } },
         { type: 'Wheel', manufacturer: 'Michelin', model: 'X100', price: 200 }
       ]
-    } }
+    }
     assert_response :created
     car = user.cars.last
     assert_equal 2, car.parts.count
