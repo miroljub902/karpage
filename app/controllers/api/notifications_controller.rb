2ed0cfdaa86410012ac806678a86489668371a90
Ivan Vega <ivanvega@gmail.com>
2017-09-04 21:44:21 -0500

    (API) Fix notifications

--- app/controllers/api/notifications_controller.rb
+++ app/controllers/api/notifications_controller.rb
@@ -4,7 +4,7 @@ class Api::NotificationsController < ApiController
   before_action :require_user
 
   def index
-    @notifications = current_user.notifications.joins(:source).recent.page(params[:page]).per(params[:per] || 10)
+    @notifications = current_user.notifications.recent.page(params[:page]).per(params[:per] || 10)
     respond_with :api, @notifications
     reset_new_stuff @notifications, owner: current_user
   end
