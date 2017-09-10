a512c9d0c979cc392c5ba4db015abce37194bf7d
Ivan Vega <ivanvega@gmail.com>
2017-09-10 14:20:42 -0500

    (API) Fix user tests

--- test/controllers/api/users_controller_test.rb
+++ test/controllers/api/users_controller_test.rb
@@ -9,9 +9,8 @@ class Api::UsersControllerTest < ApiControllerTest
     User.any_instance.stubs(:send_welcome_email)
     mock_request %w[s3 ga]
     assert_difference 'User.count' do
-      post :create, params: {
+      post :create,
         user: { login: Faker::Internet.user_name.tr('.', '-'), email: Faker::Internet.email, password: 'password' }
-      }
       assert_response :created
     end
   end
@@ -19,7 +18,7 @@ class Api::UsersControllerTest < ApiControllerTest
   test 'disallow duplicate email' do
     existing = users(:john_doe)
     assert_no_difference 'User.count' do
-      post :create, params: { user: { email: existing.email, login: Faker::Internet.user_name, password: 'password' } }
+      post :create, user: { email: existing.email, login: Faker::Internet.user_name, password: 'password' }
       assert_response :unprocessable_entity
     end
   end
@@ -27,7 +26,7 @@ class Api::UsersControllerTest < ApiControllerTest
   test 'disallow duplicate login' do
     existing = users(:john_doe)
     assert_no_difference 'User.count' do
-      post :create, params: { user: { email: Faker::Internet.email, login: existing.login, password: 'password' } }
+      post :create, user: { email: Faker::Internet.email, login: existing.login, password: 'password' }
       assert_response :unprocessable_entity
     end
   end
@@ -43,22 +42,22 @@ class Api::UsersControllerTest < ApiControllerTest
         opts[:value] == 1
     end
     @request.headers['User-Agent'] = 'Mozilla/5.0 (Linux; Android 5.1.1; wv) Version/4.0 Chrome/48.0.2564.106'
-    post :create, params: {
+    post :create,
       user: {
         login: Faker::Internet.user_name.tr('.', '-'),
         email: Faker::Internet.email,
         password: 'password'
       }
-    }
   end
 
   test 'does not track failed signup' do
     User.any_instance.stubs(:send_welcome_email)
     GATracker.expects(:event!).never
-    post :create, params: { user: { login: '' } }
+    post :create, user: { login: '' }
   end
 
   test 'returns whats new' do
+    mock_request :s3
     user = users(:john_doe)
     friend = users(:friend)
     NewStuff.reset_count(user.friends_posts, user, owner: nil, delay: true)
@@ -73,6 +72,7 @@ class Api::UsersControllerTest < ApiControllerTest
   end
 
   test 'returns whats new counters' do
+    mock_request :s3
     user = users(:john_doe)
     friend = users(:friend)
     NewStuff.reset_count(user.friends_posts, user, owner: nil, delay: true)
@@ -81,7 +81,7 @@ class Api::UsersControllerTest < ApiControllerTest
     user.followers << friend
     friend.posts.create! body: 'Howdy', photo_id: 'dummy'
     authorize_user user
-    put :reset_counter, params: { counter: 'friends_posts' }
+    put :reset_counter, counter: 'friends_posts'
     get :show
     assert_equal 0, json_response['user']['new_posts']
     assert_equal 1, json_response['user']['new_followers']
@@ -91,7 +91,7 @@ class Api::UsersControllerTest < ApiControllerTest
     user = users(:john_doe)
     authorize_user user
     mock_request :s3
-    patch :update, params: { user: { name: 'New Name' } }
+    patch :update, user: { name: 'New Name' }
     assert_response :ok
     assert_equal 'New Name', json_response['user']['name']
     assert_equal 'New Name', user.reload.name
@@ -102,11 +102,11 @@ class Api::UsersControllerTest < ApiControllerTest
     mock_request %i[s3 ga]
     mock_request :facebook_get_me, response: :valid_token
     assert_difference 'User.count + Identity.count', +2 do
-      post :create, params: { user: {
+      post :create, user: {
         login: Faker::Internet.user_name.tr('.', '-'),
         email: Faker::Internet.email,
         facebook_token: '123'
-      } }
+      }
       assert_response :created
     end
   end
@@ -116,11 +116,11 @@ class Api::UsersControllerTest < ApiControllerTest
     user.identities.create! provider: 'facebook', uid: '137110159998964'
     mock_request :facebook_get_me, response: :valid_token
     assert_no_difference 'User.count + Identity.count' do
-      post :create, params: { user: {
+      post :create, user: {
         login: Faker::Internet.user_name.tr('.', '-'),
         email: Faker::Internet.email,
         facebook_token: '123'
-      } }
+      }
       assert_response :unprocessable_entity
     end
   end
@@ -136,9 +136,10 @@ class Api::UsersControllerTest < ApiControllerTest
   end
 
   test 'can add dream cars' do
+    mock_request :s3
     user = users(:john_doe)
     authorize_user user
-    patch :update, params: { user: { dream_cars_attributes: [{ image_id: 'dummy' }] } }
+    patch :update, user: { dream_cars_attributes: [{ image_id: 'dummy' }] }
     assert_response :ok
     assert_equal 1, user.dream_cars.count
     assert_equal 'dummy', user.dream_cars.first.image_id
@@ -148,7 +149,7 @@ class Api::UsersControllerTest < ApiControllerTest
     user = users(:john_doe)
     authorize_user user
     setting = User::DEFAULT_PUSH_SETTINGS.first
-    patch :update, params: { user: { push_settings: { setting[0] => !setting[1] } } }
+    patch :update, user: { push_settings: { setting[0] => !setting[1] } }
     assert_equal !setting[1], user.reload.push_setting?(setting[0])
   end
 end
