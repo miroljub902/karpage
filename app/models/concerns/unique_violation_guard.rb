db81ce1b9759d5c8e581df6dddcec61b1ccaf543
Ivan Vega <ivanvega@gmail.com>
2017-09-15 17:11:26 -0500

    Workaround race conditions while validating DB uniqueness

--- app/models/concerns/unique_violation_guard.rb
+++ app/models/concerns/unique_violation_guard.rb
@@ -8,5 +8,10 @@ module UniqueViolationGuard
   rescue PG::UniqueViolation, ActiveRecord::RecordNotUnique
     errors.add :base, "Duplicate #{model_name.human.downcase}"
     false
+  rescue ActiveRecord::StatementInvalid => e
+    # Work around race condition
+    raise unless e.original_exception.is_a?(PG::InFailedSqlTransaction)
+    errors.add :base, 'Unexpected error, please try again later'
+    false
   end
 end
