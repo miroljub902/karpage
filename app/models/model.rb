3468b10f924a3d44ebbc235988f868357211e3bf
Ivan Vega <ivanvega@gmail.com>
2017-09-15 16:46:12 -0500

    Guard model creation against uniqueness race condition

--- app/models/model.rb
+++ app/models/model.rb
@@ -2,6 +2,7 @@
 
 class Model < ActiveRecord::Base
   include FriendlyId
+  include UniqueViolationGuard
 
   belongs_to :make
   has_many :trims, dependent: :destroy
@@ -9,7 +10,8 @@ class Model < ActiveRecord::Base
 
   friendly_id :name, scope: :make, use: %i[slugged scoped]
 
-  validates :name, presence: true, uniqueness: { scope: :make_id, case_sensitive: false }
+  validates :name, presence: true
+  validates :slug, uniqueness: { scope: :make_id, case_sensitive: false }
   validates :make_id, presence: true
 
   scope :official, -> { where(official: true) }
