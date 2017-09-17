2e02d394f0ee8618a0a2dcd624b1353ada352192
Ivan Vega <ivanvega@gmail.com>
2017-09-17 14:05:59 -0500

    Block requests with bad encoding Throttle account hack attempts
    (cherry picked from commit b00d311)

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -4,4 +4,29 @@ class Rack::Attack
   blocklist 'bad accept header' do |req|
     /powerpoint/.match req.env['HTTP_ACCEPT']
   end
+
+  blocklist 'bad request' do |req|
+    begin
+      CGI.unescape(req.path).encode('utf-8')
+      false
+    rescue Encoding::UndefinedConversionError
+      true
+    end
+  end
+
+  throttle('req/ip', limit: 500, period: 5.minutes) do |req|
+    req.ip
+  end
+
+  throttle('logins/ip', limit: 5, period: 20.seconds) do |req|
+    if (req.path == '/api/session' || req.path == '/session') && req.post?
+      req.ip
+    end
+  end
+
+  throttle('logins/login', limit: 5, period: 20.seconds) do |req|
+    if (req.path == '/api/session' || req.path == '/session') && req.post?
+      req.params['user_session'].try(:[], 'login').presence
+    end
+  end
 end
