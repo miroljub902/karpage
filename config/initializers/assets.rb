f94482b02685fe0da31b799b1ec37735b6c91f24
Ivan Vega <ivanvega@gmail.com>
2017-10-30 15:09:08 -0600

    Silence asset logging

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,6 +2,8 @@
 
 # Be sure to restart your server when you modify this file.
 
+Rails.application.config.assets.quiet = true
+
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.1'
 
