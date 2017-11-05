e09b9a30271119ca1f8cd8d24ab003f93c05effc
Ivan Vega <ivanvega@gmail.com>
2017-11-05 02:23:40 -0600

    Small fix

--- app/decorators/mentions_formatter.rb
+++ app/decorators/mentions_formatter.rb
@@ -5,6 +5,7 @@ module MentionsFormatter
   private
 
   def format_mentions(string, limit: MAX_MENTIONS)
+    return unless string
     string.dup.tap do |formatted|
       string.scan(REGEX).flatten.uniq[0...limit].each do |mention|
         link = link_to "@#{mention}", profile_path(mention)
