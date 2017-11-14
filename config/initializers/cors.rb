05f610c1714247e8659bec91b560d3d1ef167a39
Ivan Vega <ivanvega@gmail.com>
2017-11-14 12:15:52 -0600

    Loosen up CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins %w[karpage.com www.karpage.com karpage.dev:3000 localhost:3000]
+    origins %w[karpage.com www.karpage.com] + [%r{\Ahttp://(karpage\.dev|localhost)(:\d+)?\z}]
     resource '*',
              headers: :any,
              methods: %i[get post put patch delete options head]
