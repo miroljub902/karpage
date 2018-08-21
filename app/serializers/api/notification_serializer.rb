780a2c43d93d6e6f3f023580c962340b86f01397
Miroljub Markovic <miroljub.markovic@snipclip.com>
2018-08-21 15:59:44 +0800

    attaching image comment

--- app/serializers/api/notification_serializer.rb
+++ app/serializers/api/notification_serializer.rb
@@ -4,8 +4,13 @@ class Api::NotificationSerializer < ActiveModel::Serializer
   attributes %i[id type notifiable_id notifiable_type related_id related_type source_id source_type message created_at]
   attributes :image_url, :notifiable_image_url
   attribute :following, if: -> { object.new_follower? }
+  attribute :plain_body, if: -> { object.notifiable_type == 'Comment' }
 
   delegate :source_id, :image_url, :notifiable_image_url, :notifiable_id, :related_id, :related_type, to: :push_object
+
+  def plain_body
+    object.notifiable.body
+  end
 
   def following
     if object.notifiable.is_a?(Follow)
